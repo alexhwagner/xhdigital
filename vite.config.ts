4919bb497ada0a046f729b0e97fd6ddf3565b39f
afafss <alexsandro.wagner0208@gmail.com>
2026-02-10 09:20:26 -0300

    fix: github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "/xhdigital/",
   plugins: [react()],
   resolve: {
     alias: {
